28dba641e8840ffda48bd15119b95d58d96eab13
U-Reds-Laptop\Antonio <Antonio@Reds-Laptop.(none)>
2011-04-12 14:55:34 -0500

    Messages Work!!!, disregard all the dumb broken shit but they WORK

--- src/main/scala/code/model/Message.scala
+++ src/main/scala/code/model/Message.scala
@@ -5,21 +5,38 @@
 package code.model
 
 import net.liftweb._
+import http.SHtml
 import mapper._
 import sitemap.Loc.LocGroup
 import util._
 import common._
-import xml.NodeSeq
+import xml.Text._
+import xml.{Text, NodeSeq}
 
 class Message extends LongKeyedMapper[Message] with IdPK {
 
   def getSingleton = Message
 
   object sent_date extends MappedDate(this)
-  object text extends MappedText(this)
+  object body extends MappedText(this)
   object subject extends MappedString(this,100)
   object sender extends LongMappedMapper(this, User)
-  object receiver extends LongMappedMapper(this, User)
+  object receiver extends LongMappedMapper(this, User) {
+
+    override def asHtml = {
+      <span>
+        {User.find(By(User.id, this)).map(u => (u.firstName + " " + u.lastName)).openOr(Text("PM"))}
+      </span>
+    }
+
+    override def validSelectValues: Box[List[(Long, String)]] =
+      Full(User.findAll(OrderBy(User.userName, Ascending)).map(c => (c.id.is, c.niceName)))
+
+    override def _toForm = {
+      Full(SHtml.text("",(s : String) => this.set(User.find(By(User.userName,s)).open_!.id.is)))
+    }
+
+  }
   object sender_deleted extends MappedBoolean(this) {
     override def defaultValue = false
   }
@@ -36,4 +53,6 @@ object Message extends Message with LongKeyedMetaMapper[Message] with CRUDify[Lo
   override def calcPrefix = List("admin",_dbTableNameLC)
   override def showAllMenuLocParams = LocGroup("admin") :: Nil
   override def createMenuLocParams = LocGroup("admin") :: Nil
+
+
 }
--- src/main/scala/code/snippet/MessagesTab.scala
+++ src/main/scala/code/snippet/MessagesTab.scala
@@ -3,21 +3,29 @@
 
 package code.snippet
 
-import xml.{NodeSeq}
+import xml.{NodeSeq,Text}
 import net.liftweb._
+import common.Full
+import http.js.JsCmds.SetHtml
 import http.{SHtml, S, DispatchSnippet}
+import mapper.By
 import util.Helpers._
 import code.model.{Message, User}
 
 class MessagesTab extends DispatchSnippet{
   def dispatch = {
-    case "show" if(User.loggedIn_?) => show 
+    case "show" if(User.loggedIn_?) => show
+    case "ShowSend" if(User.loggedIn_?) => sendForm _
+  }
+
+  def sendForm(xhtml : NodeSeq) : NodeSeq = {
+    var new_message = Message.create
+    new_message.toForm(Full("Submit"), {_.save})
   }
 
   def show = {
     val my_messages : List[Message] = User.currentUser.map((u : User) => u.received_messages.all) openOr Nil
-    "li *" #> my_messages.map((m : Message) =>
-                                  "STRONG" #> m.subject & "i" #> m.text)
+    "li *" #> my_messages.map((m : Message) => "STRONG" #> m.subject & "i" #> m.body)
   }
 
 }
