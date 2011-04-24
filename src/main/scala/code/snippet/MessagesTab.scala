b8fc4bd8cf6fbb0e0c3229bccdf39cba19b166bc
U-Reds-Laptop\Antonio <Antonio@Reds-Laptop.(none)>
2011-04-23 23:12:36 -0500

    Added some stuff to the listings

--- src/main/scala/code/snippet/MessagesTab.scala
+++ src/main/scala/code/snippet/MessagesTab.scala
@@ -14,18 +14,29 @@ import code.model.{Message, User}
 
 class MessagesTab extends DispatchSnippet{
   def dispatch = {
-    case "show" => show
+    case "showReceived" => showReceived
+    case "showSent" => showSent
     case "ShowSend" => sendForm _
   }
 
-  def sendForm(xhtml : NodeSeq) : NodeSeq = {
+  def sendForm(xhtml: NodeSeq): NodeSeq = {
     var new_message = Message.create
-    new_message.toForm(Full("Submit"), {_.save})
+    new_message.toForm(Full("Submit"), {
+                                        (m: Message) => {
+                                          m.sender.set(User.currentUser.open_!.id)
+                                          m.sent_date.set(new java.util.Date)
+                                          m.save
+                                        }
+                                       })
   }
 
-  def show = {
+  def showReceived = {
     val my_messages : List[Message] = User.currentUser.map((u : User) => u.received_messages.all) openOr Nil
     "li *" #> my_messages.map((m : Message) => "h6 *" #> m.subject & "p *" #> m.body.is)
   }
 
+  def showSent = {
+    val my_messages : List[Message] = User.currentUser.map((u : User) => u.sent_messages.all) openOr Nil
+    "li *" #> my_messages.map((m : Message) => "h6 *" #> m.subject & "p *" #> m.body.is)
+  }
 }
