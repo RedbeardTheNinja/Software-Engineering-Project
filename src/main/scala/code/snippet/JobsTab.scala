cf66959ac2bbb48ef74fc295737a546a6acd87ee
U-Mini-Sub\Corey <Corey@Mini-Sub.(none)>
2011-04-24 16:57:45 -0500

    Added code for job tap

--- src/main/scala/code/snippet/JobsTab.scala
+++ src/main/scala/code/snippet/JobsTab.scala
@@ -5,8 +5,13 @@ package code.snippet
 
 import xml.{NodeSeq}
 import net.liftweb._
+import mapper.By
 import util.Helpers._
+import code.model.Listing
 
 class JobsTab {
-	def render = "*" #> <strong>this is the jobs tab</strong>
+	def render = {
+    val jobList = Listing.findAll(By(Listing.is_?, Listing.typeOfListing.Job))
+    "li *" #> jobList.map((l : Listing) => "h6 *" #> l.title & "#contact *" #> l.contact & "p *" #> l.details & "#poster *" #> l.poster)
+  }
 }
